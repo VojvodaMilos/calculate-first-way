4f0bf2854cea9a2f5aca43e038101f8d6fce0c1e
sudimac <vojvoda1988@gmail.com>
2022-09-18 11:39:04 +0200

    EVAL function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [number, setNumber] = useState("");
+
+  const addNumber = (e) => {
+    setNumber(number.concat(e.target.innerText));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <input type="text" defaultValue={number} />
+      <button onClick={addNumber}>1</button>
+      <button onClick={addNumber}>2</button>
+      <button onClick={addNumber}>3</button>
+      <button onClick={addNumber}>/</button>
+      <button onClick={addNumber}>4</button>
+      <button onClick={addNumber}>5</button>
+      <button onClick={addNumber}>6</button>
+      <button onClick={addNumber}>*</button>
+      <button onClick={addNumber}>7</button>
+      <button onClick={addNumber}>8</button>
+      <button onClick={addNumber}>9</button>
+      <button onClick={addNumber}>-</button>
+      <button onClick={addNumber}>.</button>
+      <button onClick={addNumber}>0</button>
+      <button
+        onClick={() => {
+          setNumber("");
+        }}
+      >
+        C
+      </button>
+      <button onClick={addNumber}>+</button>
+
+      <button
+        onClick={() => {
+          try {
+            let a = eval(number);
+            setNumber(a.toString());
+          } catch (error) {
+            setNumber("Error");
+          }
+        }}
+      >
+        =
+      </button>
     </div>
   );
 }
